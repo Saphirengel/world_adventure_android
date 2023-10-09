6ba2dab7775e75bd90b4039953c1411865db08ce
saphirengel <jan.baerenwald@gmail.com>
2023-10-09 15:49:16 +0200

    Lala

--- app/src/main/java/com/example/worldadventure/MainActivity.kt
+++ app/src/main/java/com/example/worldadventure/MainActivity.kt
@@ -7,15 +7,20 @@ import com.example.worldadventure.databinding.CharaktererstellenBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: CharaktererstellenBinding
-    var hero= Charaktererstellung(1..20,
+    //hier wird der Grundheld deklariert
+    var hero = Charaktererstellung(
         1..20,
         1..20,
         1..20,
         1..20,
         1..20,
-        1..20,)
+        1..20,
+        1..20,
+    )
+
+    //hier werten die Werte ausgelesen bzw wo sie herkommen
     var strenght = hero.strenght.toString()
-    var dextery  = hero.dextery.toString()
+    var dextery = hero.dextery.toString()
     var intelligence = hero.intelligence.toString()
     var constituion = hero.consttuion.toString()
     var wisdom = hero.wisdom.toString()
@@ -25,25 +30,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = CharaktererstellenBinding.inflate(layoutInflater)
         val view = binding.root
-       strenght =  binding.wertStaerkeZahl.toString()
-       dextery =  binding.wertGeschicklichkeitZahl.toString()
-       intelligence =  binding.wertIntelligenzZahl.toString()
-       constituion =  binding.wertKonstitutionZahl.toString()
-       wisdom =  binding.wertWeisheitZahl.toString()
-       charisma =  binding.wertCharismaZahl.toString()
-       luck =  binding.wertGlueckZahl.toString()
-
+        hero.zufallsZahlen()
+        //hier werden die Werte an ein Text gebunden
+        //leider nicht funktioniert es bildlich auszugeben
+        //strenght = binding.wertStaerkeZahl.toString()
+        //dextery = binding.wertGeschicklichkeitZahl.toString()
+       // intelligence = binding.wertIntelligenzZahl.toString()
+       // constituion = binding.wertKonstitutionZahl.toString()
+        //wisdom = binding.wertWeisheitZahl.toString()
+       // charisma = binding.wertCharismaZahl.toString()
+       // luck = binding.wertGlueckZahl.toString()
+        binding.textWerteZahl.
         //setContentView(R.layout.intro)
         //setContentView(R.layout.story)
         setContentView(R.layout.charaktererstellen)
-
-
-
-        hero.zufallsZahlen()
-
-
-
     }
-
 }
-
