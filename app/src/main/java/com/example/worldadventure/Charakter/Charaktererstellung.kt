df3f8960d5b9e5453bb3f706e61082f7e327c676
Jan <jan.baerenwald@web.de>
2023-10-11 14:26:31 +0200

    Soweit

--- app/src/main/java/com/example/worldadventure/Charakter/Charaktererstellung.kt
+++ app/src/main/java/com/example/worldadventure/Charakter/Charaktererstellung.kt
@@ -1,5 +1,8 @@
 package com.example.worldadventure.Charakter
 
+import android.widget.TextView
+import com.example.worldadventure.databinding.CharaktererstellenBinding
+
 class Charaktererstellung(
 
     val strenghtRange: IntRange,
@@ -10,11 +13,11 @@ class Charaktererstellung(
     val charismaRange: IntRange,
     val luckRange: IntRange
 ) {
-
+    private lateinit var binding: CharaktererstellenBinding
     var strenght:Int = 0
     var dextery :Int = 0
     var intelligence : Int= 0
-    var consttuion :Int = 0
+    var constuion :Int = 0
     var wisdom: Int = 0
     var charisma: Int = 0
     var luck: Int = 0
@@ -26,10 +29,18 @@ fun zufallsZahlen() {
     strenght = strenghtRange.random()
       dextery = dexteryRange.random()
       intelligence = intelligenceRange.random()
-      consttuion = constituionRange.random()
+      constuion = constituionRange.random()
       wisdom = wisdomRange.random()
       charisma = charismaRange.random()
       luck = luckRange.random()
+
+    strenght = binding.tvStaerkeZahl.toInt()
+    dextery = binding.tvGeschicklichkeitZahl.toInt()
+    intelligence = binding.tvIntelligenzZahl.toInt()
+    constuion = binding.tvKonstitutionZahl.toInt()
+    wisdom = binding.tvWeisheitZahl.toInt()
+    charisma = binding.tvCharismaZahl.toInt()
+    luck = binding.tvGlueckZahl.toInt()
  }
 
 
@@ -37,4 +48,9 @@ fun zufallsZahlen() {
 
 
     }
+
+private fun TextView.toInt(): Int {
+  return text.toString().toIntOrNull() ?: 0
+
+}
 
--- app/src/main/java/com/example/worldadventure/MainActivity.kt
+++ app/src/main/java/com/example/worldadventure/MainActivity.kt
@@ -7,42 +7,11 @@ import com.example.worldadventure.databinding.CharaktererstellenBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: CharaktererstellenBinding
-    //hier wird der Grundheld deklariert
-    var hero = Charaktererstellung(
-        1..20,
-        1..20,
-        1..20,
-        1..20,
-        1..20,
-        1..20,
-        1..20,
-    )
 
-    //hier werten die Werte ausgelesen bzw wo sie herkommen
-    var strenght = hero.strenght.toString()
-    var dextery = hero.dextery.toString()
-    var intelligence = hero.intelligence.toString()
-    var constituion = hero.consttuion.toString()
-    var wisdom = hero.wisdom.toString()
-    var charisma = hero.charisma.toString()
-    var luck = hero.luck.toString()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = CharaktererstellenBinding.inflate(layoutInflater)
-        val view = binding.root
-        hero.zufallsZahlen()
-        //hier werden die Werte an ein Text gebunden
-        //leider nicht funktioniert es bildlich auszugeben
-        //strenght = binding.wertStaerkeZahl.toString()
-        //dextery = binding.wertGeschicklichkeitZahl.toString()
-       // intelligence = binding.wertIntelligenzZahl.toString()
-       // constituion = binding.wertKonstitutionZahl.toString()
-        //wisdom = binding.wertWeisheitZahl.toString()
-       // charisma = binding.wertCharismaZahl.toString()
-       // luck = binding.wertGlueckZahl.toString()
-        binding.textWerteZahl.
-        //setContentView(R.layout.intro)
-        //setContentView(R.layout.story)
-        setContentView(R.layout.charaktererstellen)
+        setContentView(binding.root)
     }
 }
