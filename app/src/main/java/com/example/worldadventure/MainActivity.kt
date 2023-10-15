3c7c0ede72dc993cdb1e184473aa0e5f6df4a16d
Jan <jan.baerenwald@web.de>
2023-10-15 19:31:08 +0200

    Einen Spinner implementiert noch nicht hübsch aber funktioniert

--- app/src/main/java/com/example/worldadventure/MainActivity.kt
+++ app/src/main/java/com/example/worldadventure/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.worldadventure
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.worldadventure.Charakter.Charaktererstellung
 import com.example.worldadventure.databinding.CharaktererstellenBinding
 
@@ -9,9 +11,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: CharaktererstellenBinding
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = CharaktererstellenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        var spinner:Spinner = findViewById(R.id.sp_oberrasse)
+       var adapter = ArrayAdapter.createFromResource(this, R.array.oberrassen,android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
     }
 }
